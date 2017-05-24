9c7d1f46df1248b39d68d03aa0d14f7c71a8645e
Md Farhadur Reza <farhad9952@gmail.com>
2017-05-24 11:33:38 -0500

    Add files via upload

---
+++ String_in_String/string_in_String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        std::size_t found = haystack.find(needle);
+        if(found!=std::string::npos)
+       {
+           return found;
+       }
+       else
+         return -1;
+    }
+};
