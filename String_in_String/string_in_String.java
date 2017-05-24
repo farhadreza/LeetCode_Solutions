4621ecc7836c7541025bf8dfd709ff3e9f277020
Md Farhadur Reza <farhad9952@gmail.com>
2017-05-24 11:32:26 -0500

    Add files via upload

---
+++ String_in_String/string_in_String.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        
+       if(haystack.contains(needle))
+       {
+           return haystack.indexOf(needle);
+       }
+       else
+         return -1;
+        
+    }
+}
