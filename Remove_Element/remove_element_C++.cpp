6c3042b917656eec9a208046cd236b5a992fb110
Md Farhadur Reza <farhad9952@gmail.com>
2017-05-24 10:43:10 -0500

    Add files via upload

---
+++ Remove_Element/remove_element_C++.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count=0;         
+        for(int k=0;k<nums.size();k++)     
+        {
+             if(nums[k]!=val) 
+             {
+			   nums[count]=nums[k];
+	
+               count++;
+			 }
+        }
+		return count;
+        
+    }
+};
