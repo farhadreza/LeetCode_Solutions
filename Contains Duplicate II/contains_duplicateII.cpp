cd536dfec82013d68a24c65272f9cc36a6bbf507
Md Farhadur Reza <farhad9952@gmail.com>
2017-06-26 21:02:51 -0500

    Add files via upload

---
+++ Contains Duplicate II/contains_duplicateII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(nums.size()<=1)
+          return false;
+
+         unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {           
+         if(mp.find(nums[i])!=mp.end() && abs(mp[nums[i]]-i)<=k)
+         {
+             return true;
+         }
+         
+         else
+           mp[nums[i]]=i;
+ 
+        }
+ 
+        return false;
+    }
+    
+    
+};
